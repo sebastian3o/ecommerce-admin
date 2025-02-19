3296ea578f30bd0b12456621c59fa090ba4ee6a0
sebastian3o <barbasebastian30@gmail.com>
2025-02-19 14:31:08 +0200

    Modal components for Admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
 
 
 const geistSans = Geist({
@@ -30,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ModalProvider></ModalProvider>
         {children}
       </body>
     </html>
