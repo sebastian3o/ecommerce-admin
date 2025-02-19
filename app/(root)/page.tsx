112b18c944173b2f7f1743c3f9b8bde572bb48a6
sebastian3o <barbasebastian30@gmail.com>
2025-02-19 11:10:00 +0200

    Clerk Authentication for Admin

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+
+const SetupPage=()=>  {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/"></UserButton>
+    
+    </div>
+  );
+}
+
+
+export default SetupPage;
