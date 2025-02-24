1243bd34e99515ffcbce3d2cc896acc413d6bfe2
sebastian3o <barbasebastian30@gmail.com>
2025-02-24 11:59:19 +0200

    Settings page functionality for Admin

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -11,6 +11,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { Form, FormControl, FormField, FormLabel,FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
 
 interface SettingsFormProps{
     initialData:Store;
@@ -27,6 +32,8 @@ export const SettingsForm:React.FC<SettingsFormProps> = ({
     initialData
     })=>{
 
+        const params = useParams()
+        const router = useRouter()
 
         const[open,setOpen]=useState(false)
         const [loading, setLoading] = useState(false)
@@ -36,11 +43,43 @@ export const SettingsForm:React.FC<SettingsFormProps> = ({
     })
 
     const onSubmit = async (data:SettingsFormValues)=>{
-        console.log(data);
+       try{
+        setLoading(true)
+        await axios.patch(`/api/stores/${params.storeId}`,data)
+        router.refresh()
+        toast.success("Store updated.")
+       }catch(error)
+       {
+        toast.error("Something went wrong.")
+       }finally{
+        setLoading(false)
+       }
+    }
+
+    const onDelete = async ()=>{
+        try{
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh()
+            router.push("/")
+            toast.success("Store deleted.")
+        }catch(error){
+            toast.error("Make sure you remove all products and categories first.")
+        }finally{
+            setLoading(false)
+            setOpen(false)
+        }
     }
 
     return (
         <>
+            <AlertModal 
+            isOpen={open}
+            onClose={()=>setOpen(false)}
+            onConfirm={onDelete}
+            loading={loading}>
+
+            </AlertModal>
             <div className="flex items-center justify-between">
                 <Heading
                 title="Settings"
@@ -89,7 +128,12 @@ export const SettingsForm:React.FC<SettingsFormProps> = ({
 
                 </form>
             </Form>
-
+            <Separator/>
+            <ApiAlert 
+            title="NEXT_PUBLIC_API_URL"
+            description={`${origin}/api/${params.storeId}`}
+            variant="public"
+            />
         </>
 
     )
