c3bfef4e4fbb1c691830c0171bb37c181ff9e502
sebastian3o <barbasebastian30@gmail.com>
2025-02-27 12:22:37 +0200

    Billboards Entity for Admin

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -17,7 +17,7 @@ export default async function DashboardLayout({
 
     const store=await prismadb.store.findFirst({
         where: {
-            id: params.storeId,
+            id: (await params).storeId,
             userId
         }
         
