d8bda3e576c817f3733b52114115cf49863cc40d
sebastian3o <barbasebastian30@gmail.com>
2025-03-01 13:32:03 +0200

    Categories Entity for Admin

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -6,7 +6,7 @@ import { CellAction } from "./cell-action"
 export type BillboardColumn = {
   id: string
   label: string
-  createdAT:string
+  createdAt:string
 }
 
 export const columns: ColumnDef<BillboardColumn>[] = [
